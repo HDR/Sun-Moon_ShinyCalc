f137fe390157324bf666a4f4b02ad8de971091eb
MrHDR <refshet@yahoo.no>
2017-01-10 13:32:47 +0100

    changes

--- app/src/main/java/com/hdr/shinycalculator/ShinyCalc.java
+++ app/src/main/java/com/hdr/shinycalculator/ShinyCalc.java
@@ -14,7 +14,7 @@ public class ShinyCalc extends AppCompatActivity {
     int counter = 0;
 
     TextView currChance;
-    int encChance = 1/4096;
+    char encChance = 1/4096;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +25,41 @@ public class ShinyCalc extends AppCompatActivity {
         currChance = (TextView) findViewById(R.id.encChance);
 
         Spinner dropdown = (Spinner) findViewById(R.id.modeSet);
-        String[] items = new String[]{"Matsuda", "S.O.S"};
+        String[] items = new String[]{"Matsuda", "S.O.S", "Soft Reset"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
     }
 
     public void increaseCount(View view) {
-        counter++;
-        encValue.setText(Integer.toString(counter));
+          counter++;
+          encValue.setText(Integer.toString(counter));
          if (counter <= 69) {
-            encChance = 1/4096;
-            currChance.setText(Integer.toString(encChance));
-            if (counter >= 69) {
-                encChance = 1/1024;
-                currChance.setText(Integer.toString(encChance));
-            }
+             currChance.setText("1/4096");
          }
+        if (counter >= 70 && counter <= 255) {
+            currChance.setText("1/1024");
         }
+        if (counter >= 256 && counter <= 325) {
+            currChance.setText("1/4096");
+
+        }
+        if (counter >= 325 && counter <= 510) {
+            currChance.setText("1/1024");
+
+        }
+        if (counter >= 510 && counter <= 580) {
+            currChance.setText("1/4096");
+        }
+        if (counter >= 580 && counter <= 765) {
+            currChance.setText("1/1024");
+        }
+        if (counter >= 765 && counter <= 835) {
+            currChance.setText("1/4096");
+        }
+        if (counter >= 835 && counter <= 1020) {
+            currChance.setText("1/1024");
+        }
+     }
 
     public void decreaseCount(View view) {
         if (counter == 0) {
@@ -50,24 +68,41 @@ public class ShinyCalc extends AppCompatActivity {
         else if (counter >= 1) {
             counter--;
             encValue.setText(Integer.toString(counter));
+            if (counter <= 69) {
+                currChance.setText("1/4096");
+            }
+            if (counter >= 70 && counter <= 255) {
+                currChance.setText("1/1024");
+            }
+            if (counter >= 256 && counter <= 325) {
+                currChance.setText("1/4096");
+
+            }
+            if (counter >= 325 && counter <= 510) {
+                currChance.setText("1/1024");
+
+            }
+            if (counter >= 510 && counter <= 580) {
+                currChance.setText("1/4096");
+            }
+            if (counter >= 580 && counter <= 765) {
+                currChance.setText("1/1024");
+            }
+            if (counter >= 765 && counter <= 835) {
+                currChance.setText("1/4096");
+            }
+            if (counter >= 835 && counter <= 1020) {
+                currChance.setText("1/1024");
+            }
         }
     }
 
     public void resetCount(View view) {
         counter = 0;
         encValue.setText(String.valueOf(counter));
+        currChance.setText("1/4096");
     }
 
-    public void currChanceCount (View view) { //Ignore This
-     if (counter <= 69) {
-        encChance = 1 / 4096;
-        currChance.setText(Integer.toString(encChance));
-        if (counter >= 69) {
-            encChance = 1 / 1024;
-            currChance.setText(Integer.toString(encChance));
-        }
-     }
-    }
 
 
     }
